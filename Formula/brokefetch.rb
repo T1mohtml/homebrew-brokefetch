eb7091dbd0c6cfa4ee3e84d73436e8cf8966e44b
T1mohtml <timon.black.gamig@gmail.com>
2025-08-06 20:26:14 +0200

    aaa

--- Formula/brokefetch.rb
+++ Formula/brokefetch.rb
@@ -4,6 +4,7 @@ class Brokefetch < Formula
   url "https://github.com/T1mohtml/brokefetch/archive/refs/tags/re-release1.7.tar.gz"
   sha256 "21a6f81a7b0232c68b1d839d689d1efd5220909e753d4985322873148e34cb5e"
   license "MIT"
+  version "1.7.1"
 
   depends_on "bash"
 
