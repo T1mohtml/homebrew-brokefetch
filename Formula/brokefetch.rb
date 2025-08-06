e3991f6bbfa40beff6e5f747b56ab522bea440f2
T1mohtml <timon.black.gamig@gmail.com>
2025-08-06 15:31:20 +0200

    change brokefetch.rb to update the formula

--- Formula/brokefetch.rb
+++ Formula/brokefetch.rb
@@ -2,7 +2,7 @@ class Brokefetch < Formula
   desc "Funny minimal fetch script"
   homepage "https://github.com/T1mohtml/brokefetch"
   url "https://github.com/T1mohtml/brokefetch/archive/refs/tags/release1.7.tar.gz"
-  sha256 "fa8448b9c42e3de896a64fae08c0bc64deb0473ca7c9837f5e26fbe1c5ac46cd"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
   depends_on "bash"
