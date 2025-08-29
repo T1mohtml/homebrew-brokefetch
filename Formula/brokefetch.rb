85dd957197623263927fa86d5af82bed91157a47
T1mohtml <timon.black.gamig@gmail.com>
2025-08-29 08:15:29 +0200

    change smth

--- Formula/brokefetch.rb
+++ Formula/brokefetch.rb
@@ -4,6 +4,7 @@ class Brokefetch < Formula
   url "https://github.com/Szerwigi1410/brokefetch/archive/refs/tags/2025.08.27-1.tar.gz"
   sha256 "d6d59a45511317b93efdaee6636ce914aae18069b6434a5a21cda0a4e72c8d37"
   license "MIT"
+  version "2025.08.27-1.1"
 
   depends_on "bash"
 
