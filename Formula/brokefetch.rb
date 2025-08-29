c6f7c7d3a45bfa6f7258d486280a0cc47fa94a29
T1mohtml <timon.black.gamig@gmail.com>
2025-08-29 08:14:53 +0200

    fix: update URL and SHA256 for brokefetch to use tar.gz format

--- Formula/brokefetch.rb
+++ Formula/brokefetch.rb
@@ -1,10 +1,9 @@
 class Brokefetch < Formula
   desc "Funny minimal fetch script"
   homepage "https://github.com/T1mohtml/brokefetch"
-  url "https://github.com/Szerwigi1410/brokefetch/archive/refs/tags/2025.08.27-1.zip"
-  sha256 "60d844aa575429e6333192b9dffe47bed6b4a63b17880462eecea5f002f0ff17"
+  url "https://github.com/Szerwigi1410/brokefetch/archive/refs/tags/2025.08.27-1.tar.gz"
+  sha256 "d6d59a45511317b93efdaee6636ce914aae18069b6434a5a21cda0a4e72c8d37"
   license "MIT"
-  version "2025.08.27-1"
 
   depends_on "bash"
 
@@ -16,6 +15,6 @@ class Brokefetch < Formula
   end
 
   test do
-    assert_match "BROKEFETCH", shell_output("#{bin}/brokefetch")
+    assert_match "BROKEFETCH", shell_output(bin/"brokefetch")
   end
 end
