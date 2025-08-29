d15cd7ff8a9d2d1088d54cdaa6a495a74f934677
T1mohtml <timon.black.gamig@gmail.com>
2025-08-29 08:02:51 +0200

    update brokefetch to version 2025.08.27-1 with new URL and SHA256

--- Formula/brokefetch.rb
+++ Formula/brokefetch.rb
@@ -1,16 +1,18 @@
 class Brokefetch < Formula
   desc "Funny minimal fetch script"
   homepage "https://github.com/T1mohtml/brokefetch"
-  url "https://github.com/T1mohtml/brokefetch/archive/refs/tags/release1.7.4.zip"
-  sha256 "01faaba3abfe2b11ecd8b74f1b70bf753940d552ec56c9ec52f6b3db8d50dc63"
+  url "https://github.com/Szerwigi1410/brokefetch/archive/refs/tags/2025.08.27-1.zip"
+  sha256 "60d844aa575429e6333192b9dffe47bed6b4a63b17880462eecea5f002f0ff17"
   license "MIT"
-  version "1.7.4.1"
+  version "2025.08.27-1"
 
   depends_on "bash"
 
   def install
     bin.install "brokefetch.sh" => "brokefetch"
     bin.install "brokefetch_beta.sh" => "brokefetch-beta"
+    bin.install "brokefetch_mod.sh" => "brokefetch-mod"
+    pkgshare.install "logos"
   end
 
   test do
